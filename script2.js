6f886a774782176f306b9ea3ef1483eab3682431
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2024-01-14 16:37:19 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,18 @@
 alert ('よく見つかりました！この先飯テロ注意！');
+
+function showImage(src) {
+    var overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    
+    var enlargedImage = document.createElement('img');
+    enlargedImage.src = src;
+    enlargedImage.className = 'enlarged-image';
+
+    overlay.appendChild(enlargedImage);
+    
+    overlay.onclick = function () {
+        overlay.remove();
+    };
+    
+    document.body.appendChild(overlay);
+}
