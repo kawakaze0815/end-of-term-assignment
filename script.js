399eff28ecaa3e8a38ba520cf3e48b0dfef76d31
kawakaze0815 <130416324+kawakaze0815@users.noreply.github.com>
2024-01-14 14:13:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,17 @@ document.addEventListener('DOMContentLoaded', function () {
     footer.style.textAlign = 'center';
     footer.style.color = '#fff';
 });
+
+function imgchange1() {
+    document.getElementById('logo').src = './image/tea.png';
+}
+
+function imgchange2() {
+    document.getElementById('logo').src = './image/cha.png';
+}
+
+function redirectToURL(url) {
+    if (document.getElementById('logo').src.includes('tea.png')) {
+        window.location.href = url;
+    }
+}
